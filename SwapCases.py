4a945548af5d0d9d90f653e9ff8dd0e5b78ffe0b
Shalini321 <singh.shalini.mca0609@gmail.com>
2025-09-08 09:29:02 +0100

    program for case swap of a text

---
+++ SwapCases.py
@@ -0,0 +1,12 @@
+# Problem Statement:
+# How to swap all uppercase characters to lowercase and vice versa?
+
+# Questions:
+# - How the user will enter the character?
+# - How it will swap?
+# - Which commands will be used to convert each other?
+
+text = input("Enter Text : ")
+print (text.swapcase())
+
+
